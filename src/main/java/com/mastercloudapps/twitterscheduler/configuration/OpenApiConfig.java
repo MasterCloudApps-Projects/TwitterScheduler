4e0fb0c19343401295d690c3de1b4f931b163a66
david-rojo <davidrojotech@gmail.com>
2021-10-26 00:41:00 +0200

    set project name in openApi page

---
+++ src/main/java/com/mastercloudapps/twitterscheduler/configuration/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.mastercloudapps.twitterscheduler.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+
+		return new OpenAPI()
+				.components(new Components())
+				.info(new Info()
+						.title("Twitter Scheduler API")
+						.description("Twitter Scheduler TFM of URJC MasterCloudApps 2020"));
+	}
+}
