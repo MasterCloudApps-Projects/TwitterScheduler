f74a2b49057fa0964c04ba15ee6309b521135e93
david-rojo <davidrojotech@gmail.com>
2021-11-10 00:30:50 +0100

    set scheduled task every 3 minutes initial delay 1

--- src/main/java/com/mastercloudapps/twitterscheduler/application/service/task/TweetPublisherTask.java
+++ src/main/java/com/mastercloudapps/twitterscheduler/application/service/task/TweetPublisherTask.java
@@ -13,30 +13,32 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.togglz.core.manager.FeatureManager;
 
 import com.mastercloudapps.twitterscheduler.configuration.featureflags.Features;
+import com.mastercloudapps.twitterscheduler.domain.shared.NullableInstant;
 
 @Configuration
 @EnableScheduling
 @EnableAsync
-public class PublisherTask {
+public class TweetPublisherTask {
 	
 	//TODO use spring profiles to run only in PRO
 	
-	private static Logger logger = LoggerFactory.getLogger(PublisherTask.class);
+	private static Logger logger = LoggerFactory.getLogger(TweetPublisherTask.class);
 	
 	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 	
 	private FeatureManager featureManager;
 
-	public PublisherTask(FeatureManager featureManager) {
+	public TweetPublisherTask(FeatureManager featureManager) {
 		this.featureManager = featureManager;
 	}
 
 	@Async
-	@Scheduled(fixedRateString = "PT20S")
+	@Scheduled(fixedRateString = "${scheduler.frequency}", initialDelayString = "${scheduler.initial.delay}")
     public void execute() {
 		
 		if (featureManager.isActive(Features.SCHEDULER)){
-			logger.info("Publisher is enabled. Current time {}", dateFormat.format(new Date()));
+			//logger.info("Publisher is enabled. Current time {}", dateFormat.format(new Date()));
+			logger.info("Publisher is enabled. Current time {}", NullableInstant.now().getFormatted());
 		}
 	}
 
