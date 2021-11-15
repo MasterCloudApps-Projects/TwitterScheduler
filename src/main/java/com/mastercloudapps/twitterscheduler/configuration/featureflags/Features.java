685a9c809cf217f489e3da003b7e77ad74f00ba5
david-rojo <davidrojotech@gmail.com>
2021-11-15 02:14:52 +0100

    enable scheduled task by default

--- src/main/java/com/mastercloudapps/twitterscheduler/configuration/featureflags/Features.java
+++ src/main/java/com/mastercloudapps/twitterscheduler/configuration/featureflags/Features.java
@@ -1,10 +1,12 @@
 package com.mastercloudapps.twitterscheduler.configuration.featureflags;
 
 import org.togglz.core.Feature;
+import org.togglz.core.annotation.EnabledByDefault;
 import org.togglz.core.annotation.Label;
 
 public enum Features implements Feature {
 
+	@EnabledByDefault
 	@Label("Scheduler")
 	SCHEDULER
 }
